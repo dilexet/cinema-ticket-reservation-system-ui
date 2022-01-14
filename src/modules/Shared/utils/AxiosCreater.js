668ed0e157b039ae1effba1e57bde5a85ee23f41
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-14 15:24:39 +0300

    Added axios interceptors

--- src/modules/Shared/utils/AxiosCreater.js
+++ src/modules/Shared/utils/AxiosCreater.js
@@ -1,9 +1,51 @@
 import axios from "axios";
-import {BaseURL} from "../constants/BaseURLs";
+import {AuthorizeURL, BaseURL} from "../constants/BaseURLs";
+import {getLocalAccessToken} from "./TokenServices";
+import {refreshTokenAsyncAction} from "../../RefreshToken/actions-creator/RefreshTokenActions";
 
 const axiosInstance = axios.create({
     baseURL: BaseURL,
     timeout: 3000,
 })
+
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        if (!config.url.includes(AuthorizeURL)) {
+            const res = getLocalAccessToken();
+            if (res) {
+                config.headers = {
+                    'Authorization': `Bearer ` + res.jwt,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            const res = await refreshTokenAsyncAction();
+            if (res === true) {
+                originalRequest._retry = true;
+                const tokens = getLocalAccessToken();
+                if (tokens) {
+                    axios.defaults.headers.common['Authorization'] = 'Bearer ' + tokens.jwt;
+                    return axiosInstance(originalRequest);
+                }
+            }
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default axiosInstance;
