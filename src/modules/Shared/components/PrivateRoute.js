e8974315aca78fe504611680a35a0c006085c7ae
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-17 19:09:39 +0300

    Changed check authorize in routes

--- src/modules/Shared/components/PrivateRoute.js
+++ src/modules/Shared/components/PrivateRoute.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Navigate} from 'react-router-dom';
 import {isAuthorize} from '../utils/TokenServices';
 
 const PrivateRoute = ({component: Component, ...props}) => {
-    if (isAuthorize()) {
+    const [isAuthenticate, setIsAuthenticate] = useState(null);
+
+    useEffect(() => {
+        let cleanupFunction = false;
+
+        async function checkAuthorize() {
+            const result = await isAuthorize();
+            setIsAuthenticate(result)
+        }
+
+        checkAuthorize()
+        return () => cleanupFunction = true;
+    }, [isAuthenticate]);
+
+    if (isAuthenticate === null) {
+        return <div>Loading...</div>
+    } else if (isAuthenticate === true) {
         return <Component {...props}/>
     } else {
         return <Navigate to='/login'/>
--- src/modules/Shared/components/PublicRoute.js
+++ src/modules/Shared/components/PublicRoute.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Navigate} from 'react-router-dom';
 import {isAuthorize} from '../utils/TokenServices';
 
 const PublicRoute = ({component: Component, restricted, ...props}) => {
-    if (isAuthorize() && restricted) {
+    const [isAuthenticate, setIsAuthenticate] = useState(null);
+
+    useEffect(() => {
+        let cleanupFunction = false;
+
+        async function checkAuthorize() {
+            const result = await isAuthorize();
+            setIsAuthenticate(result)
+        }
+
+        checkAuthorize()
+        return () => cleanupFunction = true;
+    }, [isAuthenticate]);
+
+    if (isAuthenticate === null) {
+        return <div>Loading...</div>
+    } else if (isAuthenticate === true && restricted) {
         return <Navigate to='/'/>
     } else {
         return <Component {...props}/>
