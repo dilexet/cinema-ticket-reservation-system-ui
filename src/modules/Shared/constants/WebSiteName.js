5fc0aca365aefaa5addc853a15d5417d66564684
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-18 10:31:00 +0300

    Added short site name

--- src/modules/Shared/constants/WebSiteName.js
+++ src/modules/Shared/constants/WebSiteName.js
@@ -1,3 +1,2 @@
-const WEB_SITE_NAME = "Cinema ticket reservation system";
-
-export default WEB_SITE_NAME;
+export const WEB_SITE_NAME = "Cinema ticket reservation system";
+export const WEB_SHORT_SITE_NAME = "Cinema ticket reservation";
