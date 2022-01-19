6ea67aebc72f504ad5db35e3348d47e7d12ce993
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-19 18:58:49 +0300

    Changed Loading component

--- src/modules/Loading/component/Loading.jsx
+++ src/modules/Loading/component/Loading.jsx
@@ -1,8 +1,16 @@
-const Loading = () => {
+import React from 'react'
+import {Box, CircularProgress, Backdrop} from "@mui/material";
+
+const Loading = ({isLoading}) => {
     return (
-        <div>
-            Loading...
-        </div>
+        <Box marginLeft='auto' marginRight='auto'>
+            <Backdrop
+                sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}
+                open={isLoading}
+            >
+                <CircularProgress color="inherit"/>
+            </Backdrop>
+        </Box>
     )
 }
 
