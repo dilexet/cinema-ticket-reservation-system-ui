49c1086845f5b26d0f0ddcc65572da1f911fd345
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-19 19:48:49 +0300

    Added using authenticate provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import {Provider} from "react-redux";
 import {BrowserRouter} from 'react-router-dom';
 import AppContainer from "./modules/App/container/AppContainer";
 import {store} from "./modules/Shared/store";
+import {AuthenticateProvider} from "./modules/Shared/utils/AuthenticateContext";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <AppContainer/>
+                <AuthenticateProvider>
+                    <AppContainer/>
+                </AuthenticateProvider>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
