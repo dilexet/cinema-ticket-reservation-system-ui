e20af42b42101ec4a6cd5b55615fa285ae2b7697
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-19 19:50:13 +0300

    Removed unused import

--- src/modules/Shared/reducers/index.js
+++ src/modules/Shared/reducers/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from "@reduxjs/toolkit"
 import loginSlice from "../../Login/store/reducers/LoginReducer";
 import registerSlice from "../../Register/store/reducers/RegisterReducer";
-import userManagementSlice from "../../UserManagement/store/reducers/UserManagementReducer";
-
 
 export const rootReducer = combineReducers({
     login: loginSlice,
