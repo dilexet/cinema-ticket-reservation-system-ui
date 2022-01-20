07f8412711550cb12227821f276b375145b6a901
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-20 15:17:40 +0300

    Changed user schemes

--- src/modules/UserManagement/constants/CreateUserSchema.js
+++ src/modules/UserManagement/constants/CreateUserSchema.js
@@ -4,24 +4,24 @@ const createUserSchema = yup.object({
     Email: yup
         .string()
         .email("The Email field is not a valid")
-        .required('Please enter your email'),
+        .required('Please enter Email'),
     Name: yup
         .string()
         .min(5, "Username must be more than 5 characters")
         .max(16, "Username must be less than 16 characters")
-        .required('Please enter your username'),
+        .required('Please enter Username'),
     RoleName: yup
         .string()
-        .required('Please enter your username'),
+        .required('Please enter Role'),
     Password: yup
         .string()
         .min(4, "Password must be more than 4 characters")
         .max(25, "Password must be more than 25 characters")
-        .required('Please enter your password'),
+        .required('Please enter Password'),
     ConfirmPassword: yup
         .string()
         .oneOf([yup.ref("Password"), null], "The password and confirm password do not match")
-        .required('Please confirm your password')
+        .required('Please confirm password')
 });
 
 export default createUserSchema;
--- src/modules/UserManagement/constants/UpdateUserSchema.js
+++ src/modules/UserManagement/constants/UpdateUserSchema.js
@@ -4,15 +4,15 @@ const updateUserSchema = yup.object({
     Email: yup
         .string()
         .email("The Email field is not a valid")
-        .required('Please enter your email'),
+        .required('Please enter Email'),
     Name: yup
         .string()
         .min(5, "Username must be more than 5 characters")
         .max(16, "Username must be less than 16 characters")
-        .required('Please enter your username'),
+        .required('Please enter Username'),
     RoleName: yup
         .string()
-        .required('Please enter your username')
+        .required('Please enter Role')
 });
 
 export default updateUserSchema;
