f0b14e714ab44b3b8f112e29069dcf34134352b8
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-20 18:29:33 +0300

    Added auth reducer to root

--- src/modules/Shared/reducers/index.js
+++ src/modules/Shared/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from "@reduxjs/toolkit"
 import loginSlice from "../../Login/store/reducers/LoginReducer";
 import registerSlice from "../../Register/store/reducers/RegisterReducer";
 import userManagementSlice from "../../UserManagement/store/reducers/UserManagementReducer";
+import authenticateSlice from "../../Authenticate/store/reducers/AuthenticateReducer";
 
 
 export const rootReducer = combineReducers({
     login: loginSlice,
     register: registerSlice,
-    userManagement: userManagementSlice
+    userManagement: userManagementSlice,
+    authenticate: authenticateSlice,
 })
