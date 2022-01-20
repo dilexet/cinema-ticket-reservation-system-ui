e502e050e79cfa1466ea84366fdddfd261a5453f
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-20 18:29:59 +0300

    Added auth modules

---
+++ src/modules/Authenticate/store/action/AuthenticateAction.js
@@ -0,0 +1,14 @@
+import {authenticate_loading, authenticate_success, authenticate_error} from "../reducers/AuthenticateReducer"
+
+export const authenticateAsyncAction = (result) => {
+    console.log("CALL")
+    return async (dispatch) => {
+        dispatch(authenticate_loading())
+
+        if (result === true) {
+            dispatch(authenticate_success())
+        } else if (result === false) {
+            dispatch(authenticate_error())
+        }
+    }
+}
