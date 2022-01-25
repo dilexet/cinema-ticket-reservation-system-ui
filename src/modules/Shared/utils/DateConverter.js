56b801e034c11d48f0a2955cf8d05074c29d85e0
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-25 12:13:06 +0300

    Added date converter

--- src/modules/Shared/utils/DateConverter.js
+++ src/modules/Shared/utils/DateConverter.js
@@ -1,7 +1,18 @@
+import moment from "moment";
+
 export const toDateString = (date) => {
-    const dateParse = Date.parse(date);
-    if (dateParse !== null) {
-        return new Date(dateParse).toDateString();
+    const parsed = moment(date, 'DD/MM/YYYY')
+    if (parsed !== null) {
+        return parsed.format('DD/MM/YYYY')
+    } else {
+        return "";
+    }
+}
+
+export const toDateStringInputFormat = (date) => {
+    const parsed = moment(date, 'DD/MM/yyyy')
+    if (parsed !== null) {
+        return parsed.format('yyyy-MM-DD')
     } else {
         return "";
     }
