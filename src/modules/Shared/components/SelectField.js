ca97c639bca672fa962995810a1f1a96f941b333
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-26 17:12:48 +0300

    Added selected field component

---
+++ src/modules/Shared/components/SelectField.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {FormControl, FormHelperText, InputLabel, MenuItem, Select} from "@mui/material";
+
+const SelectField = ({
+                         id, value, name, label, onChange,
+                         onBlur, error, helperText, defaultField, data,
+                         ...props
+                     }) => {
+    return (
+        <FormControl
+            sx={{m: 1, minWidth: '210px'}}
+            required={true}
+            error={error}
+        >
+            <InputLabel htmlFor={id}>{label}</InputLabel>
+            <Select
+                id={id}
+                label={label}
+                name={name}
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                {...props}
+            >
+                <MenuItem value="">
+                    <em>{defaultField}</em>
+                </MenuItem>
+                {
+                    data?.map((item, index) => (
+                        <MenuItem key={index} value={item.id}>{item.name}</MenuItem>
+                    ))
+                }
+            </Select>
+            <FormHelperText style={{minWidth: '150px'}}>{helperText}</FormHelperText>
+        </FormControl>
+    )
+}
+
+export default SelectField;
