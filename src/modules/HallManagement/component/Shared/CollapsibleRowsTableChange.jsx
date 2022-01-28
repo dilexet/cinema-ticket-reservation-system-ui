85a25e5862344183ed2869c4742c0fd66243c63e
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-01-28 14:20:53 +0300

    Changed collapsible table styles

--- src/modules/HallManagement/component/Shared/CollapsibleRowsTableChange.jsx
+++ src/modules/HallManagement/component/Shared/CollapsibleRowsTableChange.jsx
@@ -9,7 +9,8 @@ import {
     TableCell,
     TableHead,
     TableRow,
-    Typography
+    Typography,
+    Grid
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
@@ -29,62 +30,82 @@ const CollapsibleRowsTableChange = ({
                                     }) => {
     return (
         <TableRow>
-            <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
+            <TableCell style={{padding: 0}} colSpan={6}>
                 <Collapse in={openRows} timeout="auto" unmountOnExit>
-                    <Box sx={{margin: 1, maxWidth: "md"}}>
-                        <Box display='block' justifyContent='center'>
-                            <Typography component="h2" variant="h6" gutterBottom color="secondary" style={{
-                                display: 'inline-block',
-                                textAlign: 'left',
-                                float: 'left'
-                            }}>
-                                Rows
-                            </Typography>
-                            <ButtonGroup style={{
-                                display: 'inline-block',
-                                textAlign: 'right',
-                                float: 'right',
-                            }}>
-                                <IconButton
-                                    aria-label='add'
-                                    onClick={handleAddClick}
-                                >
-                                    <AddIcon/>
-                                </IconButton>
-                                <IconButton
-                                    aria-label='clear'
-                                    onClick={handleRemoveClick}
-                                >
-                                    <RemoveIcon/>
-                                </IconButton>
-                            </ButtonGroup>
-                        </Box>
+                    <Box sx={{
+                        margin: 1,
+                        float: 'center',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        maxWidth: 'md'
+                    }}>
 
-                        <Table size="small" aria-label="purchases">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align='left'>id</TableCell>
-                                    <TableCell align="left"/>
-                                    <TableCell align='center'>Number row</TableCell>
-                                    <TableCell align='center'>Number of seats</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {
-                                    values.rows ?
-                                        values.rows.map((row, index) => (
-                                            <React.Fragment key={index}>
-                                                <RowChangeContainer
-                                                    values={values} errors={errors} touched={touched}
-                                                    handleChange={handleChange}
-                                                    hallManagementState={hallManagementState} handleBlur={handleBlur}
-                                                    index={index} setFieldValue={setFieldValue}
-                                                />
-                                            </React.Fragment>
-                                        )) : <></>
-                                }
-                            </TableBody>
-                        </Table>
+                        <Grid container xs={12} spacing={1}>
+                            <Grid container item xs={12}>
+                                <Grid item xs={4}>
+                                    <Typography component="h2" variant="h6" gutterBottom color="secondary" style={{
+                                        display: 'inline-block',
+                                        textAlign: 'left',
+                                        float: 'left'
+                                    }}>
+                                        Rows
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Box/>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <ButtonGroup style={{
+                                        display: 'inline-block',
+                                        textAlign: 'right',
+                                        float: 'right',
+                                    }}>
+                                        <IconButton
+                                            aria-label='add'
+                                            onClick={handleAddClick}
+                                        >
+                                            <AddIcon/>
+                                        </IconButton>
+                                        <IconButton
+                                            aria-label='clear'
+                                            onClick={handleRemoveClick}
+                                        >
+                                            <RemoveIcon/>
+                                        </IconButton>
+                                    </ButtonGroup>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Table width='auto' size="small" aria-label="purchases">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align='left'>id</TableCell>
+                                            <TableCell align="left"/>
+                                            <TableCell align='center'>Number row</TableCell>
+                                            <TableCell align='center'>Number of seats</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {
+                                            values.rows ?
+                                                values.rows.map((row, index) => (
+                                                    <React.Fragment key={index}>
+                                                        <RowChangeContainer
+                                                            values={values} errors={errors} touched={touched}
+                                                            handleChange={handleChange}
+                                                            hallManagementState={hallManagementState}
+                                                            handleBlur={handleBlur}
+                                                            index={index} setFieldValue={setFieldValue}
+                                                        />
+                                                    </React.Fragment>
+                                                )) : <></>
+                                        }
+                                    </TableBody>
+                                </Table>
+                            </Grid>
+                        </Grid>
+
+
                     </Box>
                 </Collapse>
             </TableCell>
--- src/modules/HallManagement/component/Shared/CollapsibleSeatTableChange.jsx
+++ src/modules/HallManagement/component/Shared/CollapsibleSeatTableChange.jsx
@@ -9,7 +9,8 @@ import {
     TableCell,
     TableHead,
     TableRow,
-    Typography
+    Typography,
+    Grid
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
@@ -31,62 +32,83 @@ const CollapsibleSeatTableChange = ({
                                     }) => {
     return (
         <TableRow>
-            <TableCell style={{paddingBottom: 0, paddingTop: 0}} colSpan={6}>
+            <TableCell style={{padding: 0}} colSpan={6}>
                 <Collapse in={openSeats} timeout="auto" unmountOnExit>
-                    <Box sx={{margin: 1, maxWidth: 'sm'}}>
-                        <Box display='block' justifyContent='center'>
-                            <Typography component="h2" variant="h6" gutterBottom color="secondary" style={{
-                                display: 'inline-block',
-                                textAlign: 'left',
-                                float: 'left'
-                            }}>
-                                Seats
-                            </Typography>
-                            <ButtonGroup style={{
-                                display: 'inline-block',
-                                textAlign: 'right',
-                                float: 'right',
-                            }}>
-                                <IconButton
-                                    aria-label='add'
-                                    onClick={handleAddClick}
-                                >
-                                    <AddIcon/>
-                                </IconButton>
-                                <IconButton
-                                    aria-label='clear'
-                                    onClick={handleRemoveClick}
-                                >
-                                    <RemoveIcon/>
-                                </IconButton>
-                            </ButtonGroup>
-                        </Box>
-                        <Table width='auto' size="small" aria-label="purchases">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align='left'>id</TableCell>
-                                    <TableCell align='center'>Number seat</TableCell>
-                                    <TableCell align='center'>Seat type</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {
-                                    values.rows[indexRow].seats ?
-                                        values.rows[indexRow].seats.map((seat, index) => (
-                                            <React.Fragment key={index}>
-                                                <SeatRowChangeContainer
-                                                    values={values} errors={errors} touched={touched}
-                                                    handleChange={handleChange}
-                                                    hallManagementState={hallManagementState}
-                                                    seatTypeState={seatTypeState}
-                                                    handleBlur={handleBlur}
-                                                    index={index} indexRow={indexRow} setFieldValue={setFieldValue}
-                                                />
-                                            </React.Fragment>
-                                        )) : <></>
-                                }
-                            </TableBody>
-                        </Table>
+                    <Box sx={{
+                        margin: 1,
+                        float: 'center',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        maxWidth: 'sm'
+                    }}>
+
+                        <Grid container xs={12} spacing={1}>
+                            <Grid container item xs={12}>
+                                <Grid item xs={4}>
+                                    <Typography component="h2" variant="h6" gutterBottom color="secondary" style={{
+                                        display: 'inline-block',
+                                        textAlign: 'left',
+                                        float: 'left'
+                                    }}>
+                                        Seats
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <Box/>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <ButtonGroup style={{
+                                        display: 'inline-block',
+                                        textAlign: 'right',
+                                        float: 'right',
+                                    }}>
+                                        <IconButton
+                                            aria-label='add'
+                                            onClick={handleAddClick}
+                                        >
+                                            <AddIcon/>
+                                        </IconButton>
+                                        <IconButton
+                                            aria-label='clear'
+                                            onClick={handleRemoveClick}
+                                        >
+                                            <RemoveIcon/>
+                                        </IconButton>
+                                    </ButtonGroup>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Table width='auto' size="small" aria-label="purchases">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align='left'>id</TableCell>
+                                            <TableCell align='center'>Number seat</TableCell>
+                                            <TableCell align='center'>Seat type</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {
+                                            values.rows[indexRow].seats ?
+                                                values.rows[indexRow].seats.map((seat, index) => (
+                                                    <React.Fragment key={index}>
+                                                        <SeatRowChangeContainer
+                                                            values={values} errors={errors} touched={touched}
+                                                            handleChange={handleChange}
+                                                            hallManagementState={hallManagementState}
+                                                            seatTypeState={seatTypeState}
+                                                            handleBlur={handleBlur}
+                                                            index={index} indexRow={indexRow}
+                                                            setFieldValue={setFieldValue}
+                                                        />
+                                                    </React.Fragment>
+                                                )) : <></>
+                                        }
+                                    </TableBody>
+                                </Table>
+                            </Grid>
+                        </Grid>
+
+
                     </Box>
                 </Collapse>
             </TableCell>
