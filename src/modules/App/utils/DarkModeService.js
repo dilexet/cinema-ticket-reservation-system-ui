bea93d69474c9a075dd5a10a26de8b873ce6c629
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-04 15:14:24 +0300

    Changed dark mode service

--- src/modules/App/utils/DarkModeService.js
+++ src/modules/App/utils/DarkModeService.js
@@ -3,6 +3,10 @@ import {createTheme} from "@mui/material";
 export const darkTheme = createTheme({
     palette: {
         mode: 'dark',
+        background: {
+            default: "#27272A",
+            paper: "#27272A"
+        }
     },
 });
 
