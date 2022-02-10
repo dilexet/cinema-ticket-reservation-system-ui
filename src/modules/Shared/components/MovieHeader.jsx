1ec90122ea110be12182189317ac5e312d4f7e6f
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-10 17:53:28 +0300

    Changed movie header

--- src/modules/Shared/components/MovieHeader.jsx
+++ src/modules/Shared/components/MovieHeader.jsx
@@ -20,12 +20,17 @@ const MovieHeader = ({movieName, theme, handleClose}) => {
                     maxWidth: '1300px',
                     boxSizing: 'border-box',
                     position: 'relative',
-                    background: 'rgba(39, 39, 42, 0.9)',
+                    background: theme.palette.mode === 'dark' ?
+                        'rgba(39, 39, 42, 0.9)' :
+                        'rgba(211, 211, 208, 0.9)'
                 }}>
                 <Box style={{boxSizing: 'border-box'}}>
                     <IconButton aria-label="Back" onClick={handleClose}
                                 style={{
-                                    display: 'inline-block', verticalAlign: 'top', color: 'rgba(255, 255, 255, 0.35)'
+                                    display: 'inline-block', verticalAlign: 'top',
+                                    color: theme.palette.mode === 'dark' ?
+                                        'rgba(255, 255, 255, 0.35)' :
+                                        'rgba(0, 0, 0, 0.35)'
                                 }}>
                         <KeyboardBackspaceIcon/>
                     </IconButton>
