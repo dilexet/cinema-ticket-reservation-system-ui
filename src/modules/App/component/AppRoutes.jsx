4059e4a054a8468ea9a5d430e606228a961a2506
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-11 13:35:53 +0300

    Changed app routes

--- src/modules/App/component/AppRoutes.jsx
+++ src/modules/App/component/AppRoutes.jsx
@@ -22,8 +22,8 @@ import HallManagementContainer from "../../HallManagement/container/HallManageme
 import AdditionalServiceManagementContainer
     from "../../AdditionalServiceManagement/container/AdditionalServiceManagementContainer";
 import SessionManagementContainer from "../../SessionManagement/container/SessionManagementContainer";
-import BookingPage from "../../BookingPage/component/BookingPage";
 import BookingContainer from "../../BookingPage/container/BookingContainer";
+import ConfirmBookingPageContainer from "../../ConfirmBookingPage/container/ConfirmBookingPageContainer";
 
 const AppRoutes = ({isAuthenticate, setIsAuthenticate, isLoading}) => {
     return (
@@ -60,6 +60,12 @@ const AppRoutes = ({isAuthenticate, setIsAuthenticate, isLoading}) => {
                                        setIsAuthenticate={setIsAuthenticate}
                                        isLoading={isLoading}
                                        component={BookingContainer}
+                                       restricted={false}/>}/>
+            <Route path='/confirm-order/movieId=:movieId/sessionId=:sessionId'
+                   element={<RoleRoute isAuthenticate={isAuthenticate} role={UserRole}
+                                       setIsAuthenticate={setIsAuthenticate}
+                                       isLoading={isLoading}
+                                       component={ConfirmBookingPageContainer}
                                        restricted={false}/>}/>
             <Route exact path='/dashboard/*'
                    element={<PrivateRoute isAuthenticate={isAuthenticate}
