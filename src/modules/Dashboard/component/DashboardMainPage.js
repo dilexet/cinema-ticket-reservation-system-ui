dc78cbc7c3bed4fbcac2ddc7f190751d22012bdb
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-14 18:35:14 +0300

    Added Dashboard main page

---
+++ src/modules/Dashboard/component/DashboardMainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box} from "@mui/material";
+
+// TODO:
+const DashboardMainPage = () => {
+    return (
+        <Box>
+            DashboardMainPage
+        </Box>
+    )
+}
+
+export default DashboardMainPage;
