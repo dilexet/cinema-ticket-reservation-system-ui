a119d3c210a7c6a248b62813391236524cd43926
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-15 10:56:31 +0300

    Changed dashboard main page

--- src/modules/Dashboard/component/DashboardMainPage.js
+++ src/modules/Dashboard/component/DashboardMainPage.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import {Box} from "@mui/material";
+import React, {useEffect} from 'react'
+import {Navigate} from "react-router-dom";
+import {getRole} from "../../Shared/utils/TokenServices";
+import Loading from "../../Loading/component/Loading";
+import {UserRole, ManagerRole, AdminRole} from "../../Shared/constants/RoleNames";
 
-// TODO:
 const DashboardMainPage = () => {
-    return (
-        <Box>
-            DashboardMainPage
-        </Box>
-    )
+
+    const [role, setRole] = React.useState(null);
+
+    useEffect(() => {
+        if (role == null) {
+            const userRole = getRole();
+            if (userRole === null) {
+                setRole(undefined)
+            } else {
+                setRole(userRole);
+            }
+        }
+    }, [role])
+
+    if (role === null) {
+        return <Loading isLoading={true}/>
+    } else if (role === undefined) {
+        return (<Navigate to='/login'/>)
+    } else if (role === UserRole) {
+        return (<Navigate to='/home'/>)
+    } else if (role === ManagerRole) {
+        return (<Navigate to='/dashboard/movie-management'/>)
+    } else if (role === AdminRole) {
+        return (<Navigate to='/dashboard/user-management'/>)
+    }
 }
 
 export default DashboardMainPage;
