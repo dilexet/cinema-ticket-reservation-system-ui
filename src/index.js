0c48c777f94b6fd567ca29461455bbbca3e7b78e
Maksim Pavarha <maksim.pavarha@outlook.com>
2022-02-21 16:47:23 +0300

    Added toastr

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import {BrowserRouter} from 'react-router-dom';
+import ReduxToastr from 'react-redux-toastr'
 import AppContainer from "./modules/App/container/AppContainer";
 import {store} from "./modules/Shared/store";
+import {TostrConfigurations} from "./modules/Shared/constants/TostrConfigurations";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -11,6 +13,7 @@ ReactDOM.render(
             <BrowserRouter>
                 <AppContainer/>
             </BrowserRouter>
+            <ReduxToastr {...TostrConfigurations}/>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
